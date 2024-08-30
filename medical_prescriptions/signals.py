14768b33fe247099499bf81284f59aa923a4e20e
Valéria <vvqf2016@gmail.com>
2024-08-30 19:32:17 -0300

    chore: added notification to patient after a new association

--- medical_prescriptions/signals.py
+++ medical_prescriptions/signals.py
@@ -1,6 +1,8 @@
 import requests
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+from patient_manager.models import DoctorPatientAssociation
 from .models import MedicalPrescription, AuditLog
 
 def get_expo_push_token_from_microservice(patient_id):
@@ -26,6 +28,17 @@ def send_push_notification(token, title, message):
     if response.status_code != 200:
         print(f"Erro ao enviar notificação: {response.text}")
 
+def get_doctor_name_from_service(doctor_id):
+    url = f'http://localhost:8001/api/doctors/{doctor_id}/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        doctor_name = data.get('user', {}).get('name')
+        return doctor_name
+    except requests.exceptions.RequestException as e:
+        return None
+
 @receiver(post_save, sender=MedicalPrescription)
 def log_prescription_save(sender, instance, created, **kwargs):
     if created:
@@ -43,5 +56,19 @@ def log_prescription_save(sender, instance, created, **kwargs):
             send_push_notification(
                 token=expo_push_token,
                 title="Nova Prescrição",
-                message=f"Você tem uma nova prescrição: {instance.description}"
+                message=f"Você tem uma nova prescrição adicionada"
+            )
+
+
+@receiver(post_save, sender=DoctorPatientAssociation)
+def doctor_association_notify(sender, instance, created, **kwargs):
+    if created:
+        expo_push_token = get_expo_push_token_from_microservice(instance.patient_id)
+        doctor_name = get_doctor_name_from_service(instance.doctor_id)
+        
+        if expo_push_token:
+            send_push_notification(
+                token=expo_push_token,
+                title="Novo vínculo",
+                message=f"Dr. {doctor_name} te adicionou!"
             )
